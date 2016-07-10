c63d8e0efdaae1e22c1b8ed1d3a3097b97006c58
Alexandre Bury <alexandre.bury@gmail.com>
2016-07-09 23:44:18 -0700

    Update doc for 6c6d80d

--- implementors/core/ops/trait.Drop.js
+++ implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];
+implementors['libc'] = [];implementors["zstd"] = ["impl&lt;W:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='zstd/struct.AutoFinishEncoder.html' title='zstd::AutoFinishEncoder'>AutoFinishEncoder</a>&lt;W&gt;",];implementors["zstd"] = ["impl&lt;W:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='zstd/struct.AutoFinishEncoder.html' title='zstd::AutoFinishEncoder'>AutoFinishEncoder</a>&lt;W&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
